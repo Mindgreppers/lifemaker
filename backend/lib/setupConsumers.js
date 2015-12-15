ee11b2f9d1ea62b3e4c5f32a9be1dbdf9157d463
Vaibhav Mule <vaibhavmule135@gmail.com>
2015-12-15 19:59:46 +0530

    woods and karma are updating on thanks and nothanks in smokesignal and comment

--- backend/lib/setupConsumers.js
+++ backend/lib/setupConsumers.js
@@ -49,8 +49,8 @@ var consumers = {
   //'u-smokesignal': 'smokesignal/update',
 
   'u-smokesignal.comment': 'smokesignal/comment',
-  'u-smokesignal.comment.thanks': 'smokesignal/comment-thanks',
-  'u-smokesignal.thanks': 'smokesignal/thanks'
+  'u-smokesignal.comment.thanks': ['smokesignal/comment-thanks', 'user/woodAndKarma'],
+  'u-smokesignal.thanks': ['smokesignal/thanks', 'user/woodAndKarma']
 }
 
 io.on('connection', function(socket) {
