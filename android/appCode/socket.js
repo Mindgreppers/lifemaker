887baa5dd7ed6cae0b861a5dd25158cc74fe54fd
awesomepankaj <pankaj@mindgrep.com>
2015-12-24 15:49:48 +0530

    added atemps in socket

--- android/appCode/socket.js
+++ android/appCode/socket.js
@@ -1,7 +1,7 @@
 var io = require('socket.io-client/socket.io')
 var params = require('../config')
 
-var socket = io(params.ipAddress, {jsonp: false})
+var socket = io(params.ipAddress, {jsonp: false, reconnectionDelay: 500, reconnectionAttempts: 5})
 
 
 module.exports = socket
