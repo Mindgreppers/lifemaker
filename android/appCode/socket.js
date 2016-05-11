5a0320d2a216560fad3d789ae7fe53a93f564dde
Abhishek Bose <abhishekbose87@gmail.com>
2016-05-11 16:37:42 +0530

    Upgrade socket.io to 1.4.5 and add multiplex option to socket

--- android/appCode/socket.js
+++ android/appCode/socket.js
@@ -1,7 +1,7 @@
 var io = require('socket.io-client/socket.io')
 var params = require('../config')
 
-var socket = io(params.ipAddress, {jsonp: false, reconnection: true})
+var socket = io(params.ipAddress, {multiplex: false, jsonp: false, reconnection: true})
 
 
 module.exports = socket
