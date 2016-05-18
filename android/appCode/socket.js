7715cbcfeb21dff127fc06e48994114c49fdd202
Abhishek Bose <abhishekbose87@gmail.com>
2016-05-18 16:07:28 +0530

    Change socket config

--- android/appCode/socket.js
+++ android/appCode/socket.js
@@ -1,7 +1,7 @@
 var io = require('socket.io-client/socket.io')
 var params = require('../config')
 
-var socket = io(params.ipAddress, {multiplex: false, jsonp: false, reconnection: true})
+var socket = io(params.ipAddress, {jsonp: false, reconnection: true, transports: ['websocket']})
 
 
 module.exports = socket
